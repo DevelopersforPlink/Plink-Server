0019dc5544796c4a3712e2c0b307449ad91a9ecd
largerejade <lsj31200@naver.com>
2025-02-08 20:51:20 +0900

    [#1] 유저 models.py 설계

--- users/models.py
+++ users/models.py
@@ -6,10 +6,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 class UserManager(BaseUserManager):
     def create_user(self, username, password, **kwargs):
-        user = self.model(
-            username = username,
-            password = password,
-        )
+        user = self.model(username = username, **kwargs)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -27,25 +24,17 @@ class UserManager(BaseUserManager):
         superuser.save(using=self._db)
         return superuser
 
-class UserPositionChoices(models.TextChoices):
+class ClientPositionChoices(models.TextChoices):
     INVESTOR = 'INV', '투자자'
     ENTREPRENEUR = 'ENT', '창업자'
 
 class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=30, unique=True)
-    image = models.ImageField(upload_to=f'profile/{change_filename}', blank=True)
     name = models.CharField(max_length=10)
-    phone = models.CharField(max_length=13)
-    company = models.CharField(max_length=50)
-    company_position = models.CharField(max_length=10)
-    company_email = models.EmailField(max_length=50)
-    certificate_employment = models.FileField(upload_to=f'certificate-employment/{change_filename}', blank=True)
-    is_in_company = models.BooleanField(default=False)
-    user_position = models.CharField(choices=UserPositionChoices.choices, max_length=3)
-    is_agree = models.BooleanField(default=False)
-    summit_count = models.IntegerField(default=0)
-    pt_count = models.IntegerField(default=0)
-    verification_code = models.CharField(max_length=6, blank=True)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = UserManager()
 
@@ -54,3 +43,44 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+def profile_upload_path(instance, filename):
+    return f'profile/{change_filename(filename)}'
+
+def certificate_upload_path(instance, filename):
+    return f'certificate-employment/{change_filename(filename)}'
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='client')
+    image = models.ImageField(upload_to=profile_upload_path, blank=True)
+    company = models.CharField(max_length=50)
+    company_position = models.CharField(max_length=10)
+    company_email = models.EmailField(max_length=50)
+    certificate_employment = models.FileField(upload_to=certificate_upload_path, blank=True)
+    is_in_company = models.BooleanField(default=False)
+    user_position = models.CharField(choices=ClientPositionChoices.choices, max_length=3)
+    is_agree = models.BooleanField(default=False)
+    summit_count = models.IntegerField(default=0)
+    pt_count = models.IntegerField(default=0)
+    verification_code = models.CharField(max_length=6, blank=True)
+
+    @property
+    def name(self):
+        return self.user.name
+
+    @property
+    def phone(self):
+        return self.user.phone
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='manager')
+    job = models.CharField(max_length=10)
+    hire_date = models.DateField()
+
+    @property
+    def name(self):
+        return self.user.name
+
+    @property
+    def phone(self):
+        return self.user.phone
