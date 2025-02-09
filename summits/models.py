56c064e5995cfcb0f82050e67fa220045ea7a038
largeredjade <130124523+largeredjade@users.noreply.github.com>
2025-02-09 20:09:07 +0900

    Revert "[#2] 전체 models.py 개발""

--- summits/models.py
+++ summits/models.py
@@ -1,20 +1,28 @@
 from django.db import models
-from common.models import BaseModel
+from common.models import BaseModel, BusinessTypeChoices, BusinessProgressChoices
+from users.models import Manager
 from utils.fileManger import change_filename
 
-
-class PT(BaseModel):
-    service_name = models.TextField()
-    title = models.TextField()
+class Summit(BaseModel):
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='summits')
     thumbnail = models.ImageField(upload_to='')
-    link = models.TextField()
-    total_link = models.TextField()
-    business_type = models.CharField(max_length=50)
-    summary = models.TextField()
-    summary_business_plan = models.FileField(upload_to='')
-    business_plan = models.FileField(upload_to='')
-    pitch_deck = models.FileField(upload_to='')
-    traction_data = models.FileField(upload_to='')
-    business_progress = models.CharField(max_length=20)
-    is_summit = models.BooleanField(default=False)
-    
+    title = models.CharField(max_length=30)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    description = models.TextField()
+    host = models.CharField(max_length=30)
+    management = models.CharField(max_length=30)
+    business_type = models.CharField(
+        max_length = 20,
+        choices = BusinessTypeChoices.choices,
+        default = BusinessTypeChoices.SERVICE
+    )
+    business_progress = models.CharField(
+        max_length=20,
+        choices = BusinessProgressChoices.choices,
+        default = BusinessProgressChoices.IDEA
+    )
+    min_video_length = models.DurationField()
+    max_video_length = models.DurationField()
+    other_requirements = models.TextField()
+    participant_count = models.PositiveIntegerField()
