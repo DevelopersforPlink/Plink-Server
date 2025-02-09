6dd31d65a8d41504070b4b006de6ebc5b442d98d
largerejade <lsj31200@naver.com>
2025-02-09 20:00:40 +0900

    [#2] 전체 models.py 개발

--- manages/models.py
+++ manages/models.py
@@ -1,6 +1,23 @@
+from django.conf import settings
 from django.db import models
+from common.models import BaseRequest
+from users.models import Client, Manager
+from pts.models import PT
+from summits.models import Summit
 
-from common.models import BaseModel
+class RequestStatus(models.TextChoices):
+    PENDING = "PENDING", "대기중"
+    APPROVED = "APPROVED", "승인됨"
+    REJECTED = "REJECTED", "거절됨"
 
+class PTRequest(BaseRequest):
+    pt = models.ForeignKey(PT, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+class SummitRequest(BaseRequest):
+    summit = models.ForeignKey(Summit, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+class ClientRequest(BaseRequest):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='clients')
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='managers')
--- pts/models.py
+++ pts/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from common.models import BaseModel, BusinessTypeChoices, BusinessProgressChoices
+from users.models import Client
+from summits.models import Summit
 
-# Create your models here.
+class PT(BaseModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='pts')
+    summit = models.ForeignKey(Summit, on_delete=models.CASCADE)
+    service_name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    thumbnail = models.ImageField(upload_to='')
+    link = models.URLField()
+    total_link = models.URLField()
+    summary = models.TextField()
+    summary_business_plan  = models.FileField(upload_to='')
+    business_plan = models.FileField(upload_to='')
+    pitch_deck = models.FileField(upload_to='')
+    traction_data = models.FileField(upload_to='')
+    business_type = models.CharField(
+        max_length=20,
+        choices=BusinessTypeChoices.choices,
+        default=BusinessTypeChoices.SERVICE
+    )
+    business_progress= models.CharField(
+        max_length=20,
+        choices=BusinessProgressChoices.choices,
+        default=BusinessProgressChoices.IDEA
+    )
+    is_summit = models.BooleanField(default=False)
