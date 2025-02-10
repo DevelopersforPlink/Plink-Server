40f5016109fbf03ba104627d29cb45c3a598af6d
largerejade <lsj31200@naver.com>
2025-02-10 15:54:46 +0900

    (feat) [#2] models.py 기능 수정

--- manages/models.py
+++ manages/models.py
@@ -9,9 +9,9 @@ class PTRequest(BaseRequest):
     manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='pt_requests')
 
 class SummitRequest(BaseRequest):
-    summit = models.ForeignKey(Summit, on_delete=models.CASCADE, related_name='summit_request')
-    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='summit_request')
+    summit = models.ForeignKey(Summit, on_delete=models.CASCADE, related_name='summit_requests')
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='summit_requests')
 
 class ClientRequest(BaseRequest):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='clients')
-    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='clients')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='client_requests')
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='client_requests')
